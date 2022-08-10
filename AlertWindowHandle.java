da868de7c65073fe22a20cc9137cf9de8fd7f11c
Serge Tarasov <73759315+neonagat@users.noreply.github.com>
2022-08-10 09:39:43 -0400

    Add files via upload

---
+++ AlertWindowHandle.java
@@ -0,0 +1,51 @@
+package seleniumScripts;
+
+import java.util.Iterator;
+import java.util.Set;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AlertWindowHandle {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Tests\\Selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		// Launching the site.
+		driver.get("http://demo.guru99.com/popup.php");
+		driver.manage().window().maximize();
+
+		driver.findElement(By.xpath("//*[contains(@href,'popup.php')]")).click();
+		
+		String MainWindow = driver.getWindowHandle();
+		System.out.println("MainWindow: " + MainWindow);
+
+		// To handle all new opened window.
+		Set<String> s1 = driver.getWindowHandles();
+		Iterator<String> i1 = s1.iterator();
+
+		while (i1.hasNext()) {
+			String ChildWindow = i1.next();
+			System.out.println("ChildWindow: " + ChildWindow);
+
+			if (!MainWindow.equalsIgnoreCase(ChildWindow)) {
+
+				// Switching to Child window
+				driver.switchTo().window(ChildWindow);
+				driver.findElement(By.name("emailid")).sendKeys("selenium@gmail.com");
+
+				driver.findElement(By.name("btnLogin")).click();   
+				String alertMessage = driver.findElement(By.xpath("/html/body/table/tbody/tr[6]/td/h3")).getText();
+				System.out.println(alertMessage);
+
+				// Closing the Child Window.
+				driver.close();
+			}
+		}
+		// Switching to Parent window i.e Main Window.
+		driver.switchTo().window(MainWindow);
+		driver.quit();
+	}
+
+}
